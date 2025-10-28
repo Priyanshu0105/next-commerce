2fa32a9419fdbc607db600521927e9fa6eb410e2
Priyanshu <priyanshu41619@gmail.com>
2025-10-28 23:38:26 +0530

    product schema

---
+++ backend/models/Product.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+const productSchema = mongoose.Schema({
+    title:{ 
+        type: String,
+        required: true
+    },
+    description:{
+        type: String ,
+        default: ""
+    },
+    price:{
+        type: Number ,
+        required: true
+    },
+    images:{
+        type:[String],
+        required:true
+    },
+    inventory:{
+        type: Number,
+        default: 0,
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Product = mongoose.model("Product",productSchema)
+
+module.exports = Product;
